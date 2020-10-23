4ffc145f795440c45ab5d0301473d5893a623658
Michael Martinez <zeroeclipse@live.com>
2020-10-22 20:56:01 -0700

    commit tree f049b4c8b469e94ed5a9a200a11d09623f63743e

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+    if array.size == 2
+        array.join(" and ")
+    elsif array.size > 2
+        newArray = array[0..array.size-3].join(", ") + ", "
+        newArray << array[array.size-2..array.size-1].join(", and ")
+        return newArray
+    else
+        return array.join
+    end
 end
